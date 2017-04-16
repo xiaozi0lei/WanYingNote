92d0198c165df8cf4ef0a0f843192fd35bca7255
xiaozi0lei <xiaozi0lei@gmail.com>
2017-04-16 19:15:01 +0800

    20170416 1. 实现分层和路由功能 2. 分 controller、service、view 层 3. 路由功能由 main 进行分发到对应的 controller 的 func 4. 使用模板读取对应的 view 的 html 视图文件

--- main.go
+++ main.go
@@ -1,29 +1,39 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"net/http"
 
 	"github.com/urfave/negroni"
 	"github.com/gorilla/mux"
+	"github.com/xiaozi0lei/WanYingNote/controller"
+	"log"
 )
 
 func main() {
 
+	// 返回 http.Handler
 	router := mux.NewRouter()
-	router.HandleFunc("/{name}", HomeHandler)
+
+	// 注册各个 handler，路由的功能
+	router.HandleFunc("/", controller.Index)
+	router.HandleFunc("/name", controller.GetName)
+	//router.HandleFunc("/", HomeHandler)
 
 	n := negroni.Classic()
+	// 将 http.Handler 传入中间件栈，handler 按加入顺序先后执行
 	n.UseHandler(router)
 
-	http.ListenAndServe(":3000", n)
+	//
+	log.Fatal(http.ListenAndServe(":3000", n))
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Welcome to my home page!")
-	
-	fmt.Fprintf(w, "Category: %v\n", vars)
-	//fmt.Fprintf(w, "Category: %v\n", vars["category"])
+func HomeHandler(rw http.ResponseWriter, r *http.Request) {
+	controller.Index(rw, r)
+	//vars := mux.Vars(r)
+	//w.WriteHeader(http.StatusOK)
+	//fmt.Fprintf(w, "%v", "Welcome to my home page!")
+	//
+	//fmt.Fprintf(w, "Category: %v\n", vars)
+	////fmt.Fprintf(w, "Category: %v\n", vars["category"])
 }
